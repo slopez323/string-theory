4e7938d2fa3536beb4c1550909c494a9526f6fc5
Sarah Lopez <slopez323@gmail.com>
2022-02-15 15:19:28 -0500

    ciemailify

--- main.js
+++ main.js
@@ -52,10 +52,10 @@ console.log(`\nEXCLAIM`)
 function exclaim(string) {
     let newString = ""
     for (let count = 0; count < string.length; count++) {
-        if (string[count] === "?" || string[count] === "."){
-        newString = `${newString}!`
+        if (string[count] === "?" || string[count] === ".") {
+            newString = `${newString}!`
         } else {
-        newString = `${newString}${string[count]}`
+            newString = `${newString}${string[count]}`
         }
     }
     return newString
@@ -65,12 +65,44 @@ console.log(exclaim("This is fine."))
 
 console.log(`\nREPEAT IT`)
 
-function repeatIt(string, n){
+function repeatIt(string, n) {
     let newString = ""
-    for (let count = 0; count < n; count++){
+    for (let count = 0; count < n; count++) {
         newString = newString + string
     }
     return newString
 }
 
 console.log(repeatIt("oh hi!", 8))
+
+console.log(`\nTRUNCATE`)
+
+function truncate(string) {
+    let newString = ""
+    if (string.length > 15) {
+        for (let count = 0; count < 15; count++) {
+            newString = `${newString}${string[count]}`
+        }
+        return `${newString}...`
+    } else {
+        return string
+    }
+}
+
+console.log(truncate("Well, that's just, like, your opinion man."))
+
+console.log(`\nCI EMAILIFY`)
+
+function ciEmailify(name) {
+    let newString = ""
+    for (let count = 0; count < name.length; count++) {
+        if (name[count] === " ") {
+            newString = `${newString}.`
+        } else {
+            newString = `${newString}${name[count]}`
+        }
+    }
+        return `${newString.toLowerCase()}@codeimmersives.com`
+}
+
+console.log(ciEmailify("Anthony DeRosa"))
